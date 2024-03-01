25d555e0a69f8b1baa63e5bf4be304835bac3205
taimoorkamran <muhammadtaimoor2006@gmail.com>
2024-03-01 10:50:36 +0500

    added Assignment 4

---
+++ Assignment 4/app.js
@@ -0,0 +1,41 @@
+// Basic for Loop:
+
+// for(let i = 0; i <= 5; i++){
+//      console.log(i)
+// }
+
+// Summation with for loop:
+
+// for(let i = 0; i <= 10; i++){
+//     console.log(i + i)
+// }
+
+// Reverse Counting for Loop:
+
+for(let i = 10; i >= 0; i--){
+    console.log(i)
+}
+
+// Mulitplication Table With For Loop: 
+
+// for(let i = 0; i <= 10; i++){
+//     console.log(1 ,  " + " ,  i , " = " , i)
+// }
+
+// Conculsion:
+
+// Defination for loop: 
+
+    // For loop is Very help ful feature of javascript you can 1 task repeating more times like hundred times thousand times so you can use for loop
+
+
+// 6. Pattern Printing with For Loop:
+
+
+// for(let i = 1; i <= 5; i++ ){
+//     let z = "";
+//     for(let e = 0;e < i; e++){
+//         z += "* " 
+//     }
+//     console.log(z)
+// }
