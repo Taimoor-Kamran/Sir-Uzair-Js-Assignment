47539791f338b3955caf6664028b2f5bddd662a1
taimoorkamran <muhammadtaimoor2006@gmail.com>
2024-03-22 06:28:41 +0500

    Added Assignment 9

---
+++ Assignment 9/app.js
@@ -0,0 +1,83 @@
+// 1. • Write a loop to print numbers from 1 to 10.
+
+// for(let i = 1; i <= 10 ; i++){
+//     console.log(i)
+// }
+
+// 2. • Write a loop to print even numbers from 1 to 20.
+
+// for(let i = 1; i <= 20; i++){
+//     if(i % 2 === 0){
+//         console.log(i)
+//     }
+// }
+
+// 3. Write a loop to print odd numbers from 1 to 15.
+
+// for(let i = 15; i >= 1; i--){
+//     if(i % 2 === 0){
+//         console.log(i)
+//     }
+// }
+
+// 4. Write a loop to calculate and print the factorial of a number (let's say 5).
+
+// let number = 5;
+// let factorial = 1;
+
+// for(let i = 1; i <= number; i++){
+//     factorial *= i;
+// }
+
+// console.log("The factorial of" ,number , "is :", factorial  )
+
+// 5. Write a loop to print the multiplication table of a number (let's say 7) up to 10.
+
+// let number = 7;
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(number , " x " , i , " = ", number * i)
+// }
+
+// 6. • Write a loop to print the Fibonacci series up to the 10th term. (The Fibonacci sequence is a type series where each number is the sum of the two that precede it. It starts from 0 and 1 usually. The Fibonacci sequence is given by 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, and so on.-10)
+
+
+// 7. Write a loop to find and print the sum of digits of a number (let's say 123).
+
+// for(let i = 1; i <= 3; i++){
+//     console.log(i)
+// }
+
+// 8. Write a loop to print the reverse of a string (let's say "hello").
+
+// let string1 = "hello"
+
+// let reverseString = ""
+
+// for(let i = string1.length - 1; i >= 0; i--){
+//     reverseString = reverseString + string1[i]
+// }
+
+// console.log(reverseString)
+
+// 9. Write a loop to print the square of numbers from 1 to 10.
+ 
+// for(let i = 1; i <= 10; i++){
+//     let square = i * i
+//     console.log(square)
+// }
+
+
+// 10. • Write a loop to find and print the smallest element in an array (let's say [3, 7, 2, 9, 5]).
+
+// const array = [3, 7, 2, 9, 5];
+// let store = array[0]
+
+// for(let i = 0; i < array.length; i ++){
+//     if(array[i] > store){
+//         store = array[i]
+//     }
+// }
+
+// console.log(2)
+
