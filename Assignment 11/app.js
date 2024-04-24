f01429411b0f6b69fbefd7041b8b5426a98a069d
muhammadtaimoor <muhammadtaimoor2006@gmail.com>
2024-04-24 08:29:04 +0500

    ässignment 1'

---
+++ Assignment 11/app.js
@@ -0,0 +1,10 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let modififyArray = []
+
+for(let i = 0; i < numbers.length; i++){
+    if(numbers[i] % 2 === 0){
+        modififyArray.push(numbers[i] * 2)
+    }
+}
+
+console.log(modififyArray)
