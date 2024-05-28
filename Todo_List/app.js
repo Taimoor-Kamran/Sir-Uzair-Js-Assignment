98b17d1a058a17480c23fe6f9f1fde69d4983491
muhammadtaimoor <muhammadtaimoor2006@gmail.com>
2024-05-28 15:28:30 +0500

    update todo list

--- Todo_List/app.js
+++ Todo_List/app.js
@@ -11,18 +11,31 @@
 // console.log('child2 nextElementSibling==>',child2.nextElementSibling)
 // // console.log('child2==>',child2.parentElement.parentElement.parentElement)
 
+let todo_input = document.getElementById("todo_input");
+let add_todo_btn = document.getElementById("add_todo_btn");
+let delete_all_todo_btn = document.getElementById("delete_all_todo_btn");
+let todo_list = document.getElementById("todo_list");
 
-let todo_input = document.getElementById('todo_input')
-let add_todo_btn = document.getElementById('add_todo_btn')
-let delete_all_todo_btn = document.getElementById('delete_all_todo_btn')
-let todo_list = document.getElementById('todo_list')
+add_todo_btn.addEventListener("click", function () {
+  if (!todo_input.value) return alert("Todo Ma Kuch Nahi ha");
 
+  var list_items = `<li><span>${todo_input.value}</span>
+  <button class='btnEdit' onclick="edit(this)">edit</button>
+  <button class='btnDelete' onclick="deletefun(this)">del</button>
+  </li>
+        `;
+  todo_list.innerHTML += list_items;
 
-add_todo_btn.addEventListener('click', function(){
-    if(!todo_input.value) return alert('Todo Ma Kuch Nahi ha')
+  todo_input.value = "";
+});  
 
-        var list_items = `<li class='todo_items'><span>${todo_input.value}</span></li>`
-        todo_list.innerHTML += list_items
+function deletefun(element) {
+  //   console.log("delete=>", element);
+  element.parentElement.remove();
+}
 
-        todo_input.value = ''
-    })
+function edit(element) {
+  var previousValue = element.previousElementSibling.innerText;
+  var updatedValue = prompt("Edit Value", previousValue);
+  element.previousElementSibling.innerText = updatedValue;
+}
