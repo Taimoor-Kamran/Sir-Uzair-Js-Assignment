7a5539f31a293706512491d22f15a21d36353e01
muhammadtaimoor <muhammadtaimoor2006@gmail.com>
2024-05-29 05:24:57 +0500

    updated Text Editor and Objext

--- Quill Text Editor/app.js
+++ Quill Text Editor/app.js
@@ -1,34 +1,93 @@
-let bold = document.getElementById("bold")
-let italic = document.getElementById("italic")
-let underline = document.getElementById('underline')
-let para = document.getElementById('para')
-let left = document.getElementById('left') 
-let justify = document.getElementById('justify')
-let dropDown = document.getElementById('dropDown')
-let editor = document.getElementById('editor')
-let dropDown1 = document.getElementById('dropDown1')
+let text = document.getElementById('text')
+let btns = document.getElementsByClassName('action_btn')
+let fontOptions = document.getElementById('font-family')
+let fontSizeOptions = document.getElementById('font-size')
 
-bold.addEventListener('click', function(){
-    para.style.fontWeight = 'bold';
+
+fontSizeOptions.addEventListener('çhange', function(){
+    if(this.value === 'heading'){
+        text.style.fontSize = '40px';
+    }
+    if(this.value === 'subHeading'){
+        text.style.fontSize = '25px';
+    }
+    if(this.value === 'normal'){
+        text.style.fontSize = '16px';
+    }
 })
-italic.addEventListener('click', function(){
-    para.style.fontStyle = 'italic'
+
+fontOptions.addEventListener('change', function(){
+    console.log(this.value)
+    if(this.value === 'monospace'){
+        text.style.fontFamily = "monospace"
+    }
+    if(this.value === 'serif'){
+        text.style.fontFamily = "serif"
+    }
+    if(this.value === 'san-serif'){
+        text.style.fontFamily = "sans-serif"
+    }
+
 })
-underline.addEventListener('click', function(){
-    para.style.textDecoration = 'underline'
-})
-left.addEventListener('click', function(){
-    para.style.textAlignLast = 'left'
-})
-justify.addEventListener('click', function(){
-    para.style.textAlignLast = 'center'
-})
-right.addEventListener('click', function(){
-    para.style.textAlignLast = 'right'
-})
-dropDown.addEventListener('click', function(){
-    para.style.fontSize = '38px'
-})
-dropDown1.addEventListener('click', function(){
-    para.style.fontFamily = 'georgia'
-})
+
+for(let i = 0; i < btns.length; i++){
+    btns[i].addEventListener('click', function(){
+        console.log("this", this)
+
+        switch (this.innerText) {
+            case 'bold':
+                if(text.style.fontWeight == 'bold'){
+                    text.style.fontWeight = 'normal'
+                }else{
+                    text.style.fontWeight = 'bold'
+                }
+                break;
+                case 'italic':
+                    if(text.style.fontStyle == 'italic'){
+                        text.style.fontStyle = 'normal'
+                    }else{
+                        text.style.fontStyle = 'italic'
+                    }
+                break;
+                case 'underline':
+                    if(text.style.textDecorationLine == 'underline'){
+                        text.style.textDecorationLine = 'normal'
+                    }else{
+                        text.style.textDecorationLine = 'underline'
+                    }
+                break;
+                case 'right':
+                    if(text.style.textAlign == 'right'){
+                        text.style.textAlign = 'normal'
+                    }else{
+                        text.style.textAlign = 'right'
+                    }
+                break;
+                case 'left':
+                    if(text.style.textAlign == 'left'){
+                        text.style.textAlign = 'normal'
+                    }else{
+                        text.style.textAlign = 'left'
+                    }
+                break;
+                case 'center':
+                    if(text.style.textAlign == 'center'){
+                        text.style.textAlign = 'normal'
+                    }else{
+                        text.style.textAlign = 'center'
+                    }
+                break;
+                case 'justify':
+                    if(text.style.textAlign == 'justify'){
+                        text.style.textAlign = 'normal'
+                    }else{
+                        text.style.textAlign = 'justify'
+                    }
+                break;
+                
+            default:
+                break;
+        }
+
+    })
+}
